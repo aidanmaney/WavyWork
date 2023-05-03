00020f35126d55857688d4f14bee4340ea31f887
Alexander Colloredo-Mansfeld <acollore@ucsc.edu>
2023-05-02 21:10:12 -0700

    Define database schema, first draft. Created tables for tasks, kanban cards, task comments, task reflections, journal entries, and groups. Worked on by Alex and Aidan

--- models.py
+++ models.py
@@ -10,6 +10,9 @@ from pydal.validators import *
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
+def get_user_id():
+    return auth.current_user.get('id') if auth.current_user else None
+
 def get_time():
     return datetime.datetime.utcnow()
 
@@ -19,5 +22,58 @@ def get_time():
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+
+# Profile
+# Tasks
+
+db.define_table(
+    'tasks',
+    Field("created_by", "integer", "reference auth_user", default=get_user_id),
+    Field("start_time", "datetime"),
+    Field("end_time", "datetime"),
+    Field("label"),
+    Field("description"),
+    Field("is_group", "boolean"),
+    Field("group_id", "reference groups")
+)
+
+db.define_table(
+    'groups',
+    Field("group_name"),
+    Field("assoc_user_1", "integer", "references auth_user"),
+    Field("assoc_user_2", "integer", "references auth_user"),
+    Field("assoc_user_3", "integer", "references auth_user"),
+    Field("assoc_user_4", "integer", "references auth_user")
+)
+
+db.define_table(
+    'kanban_cards',
+    Field("task_id", "integer", "reference tasks"),
+    Field("board_type"),     # {"personal", "school", "work"}
+    Field("column"),         # {"backlog", "todo", "in_progress", "stuck", "done"}
+)
+
+db.define_table(
+    'task_comments',
+    Field("task_id", "reference tasks"),
+    Field("comment")
+)
+
+db.define_table(
+    'task_reflections',
+    Field("task_id", "integer", "reference tasks"),
+    Field("attentiveness", "integer"),       # 1-10 based on sliders
+    Field("emotion", "integer"),             # 1-10 based on sliders
+    Field("efficiency", "integer"),          # 1-10 based on sliders
+    Field("day", "datetime")
+)
+
+db.define_table(
+    'daily_journal',
+    Field("user", "integer", "reference auth_user"),
+    Field("entry"),
+    Field("day", "datetime")
+)
+
 
 db.commit()
