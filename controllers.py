28bea8af5455c2d67f9285c1356370c9a65fc910
awsmaney1010 <86045868+awsmaney1010@users.noreply.github.com>
2023-06-05 18:41:24 -0700

    Starting functionality for profile page.

--- controllers.py
+++ controllers.py
@@ -25,6 +25,10 @@ session, db, T, auth, and templates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
+import datetime  # TODO
+import random
+from .dateutil.relativedelta import relativedelta
+
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import *
 from .common import (
@@ -45,13 +49,61 @@ url_signer = URLSigner(session)
 
 
 @action("index")
-@action.uses("index.html", db, auth)
+@action.uses("index.html", db, auth, url_signer)
 def index():
     print("User:", get_user_email())
     return dict()
 
 
+@action("get_reflections")
+@action.uses(db, auth.user, session, url_signer.verify())
+def get_reflections():
+    prev_month_offset = int(request.params.get("pmo"))
+
+    def day_n_months_ago(day, n):
+        return day + relativedelta(months=(-1 * n))
+
+    def productivity_metric(attentiveness, efficiency, emotion):
+        # TODO implement
+        return random.randint(0, 4)
+
+    today = datetime.datetime.today()
+    day_in_month = day_n_months_ago(day=today, n=prev_month_offset)
+    first_of_month = day_in_month + relativedelta(day=1)
+    last_of_month = day_in_month + relativedelta(day=31)
+
+    reflections_in_month_rows = db(
+        (db.task_reflections.day >= first_of_month)
+        & (db.task_reflections.day <= last_of_month)
+    ).select(
+        db.task_reflections.day,
+        db.task_reflections.attentiveness,
+        db.task_reflections.emotion,
+        db.task_reflection.efficiency,
+        orderby=db.task_reflections.day,
+    )
+
+    reflections_in_month = [
+        (
+            x["day"],
+            productivity_metric(
+                x["attentiveness"],
+                x["emotion"],
+                x["efficiency"],
+            ),
+        )
+        for x in reflections_in_month_rows.as_list()
+    ]
+    print(reflections_in_month)
+
+    print("REFLECTIONS_IN_MONTH:", reflections_in_month)
+
+    return dict(reflections=reflections_in_month)
+
+
 @action("profile")
-@action.uses("profile.html", db, auth.user)
+@action.uses("profile.html", db, auth.user, session, url_signer)
 def profile():
-    return dict()
+    get_reflections_url = URL("get_reflections", signer=url_signer)
+    print(get_reflections_url)
+    return dict(get_reflections_url=get_reflections_url)
--- static/js/profile.js
+++ static/js/profile.js
@@ -5,39 +5,58 @@ const app = {};
 // Given an empty app object, initializes it filling its attributes,
 // creates a Vue instance, and then initializes the Vue instance.
 const init = (app) => {
-  app.data = {
-    days_of_the_week: [
-      'Mon',
-      'Tue',
-      'Wed',
-      'Thu',
-      'Fri',
-      'Sat',
-      'Sun',
-    ],
-  };
+    app.data = {
+        daysOfTheWeek: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        // How many months *back* is the user looking
+        prevMonthOffset: 0,
+        reflections_day_level: [],
+    };
 
-  app.enumerate = (a) => {
-    let k = 0;
-    a?.map((e) => {
-      e._idx = k++;
+    app.enumerate = (a) => {
+        let k = 0;
+        a?.map((e) => {
+            e._idx = k++;
+        });
+        return a;
+    };
+
+    // app.goForwardMonth = () => {
+    //   if (app.vue.data.prevMonthOffset >= 1) {
+    //     app.vue.data.prevMonthOffset -= 1;
+    //   }
+    // };
+
+    app.get_reflections = () => {
+        axios
+            .get(get_reflections_url, {
+                params: { pmo: app.data.prevMonthOffset },
+            })
+            .then(function (response) {
+                app.data.reflections_day_level = response.data.reflections;
+                // TODO
+            });
+    };
+
+    app.methods = {
+        // goForwardMonth: app.goForwardMonth,
+        // goBackMonth: app.goBackMonth,
+        get_reflections: app.get_reflections,
+    };
+
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: function () {
+            return app.data;
+        },
+        methods: app.methods,
     });
-    return a;
-  };
 
-  app.methods = {};
+    app.init = () => {
+        app.data.prevMonthOffset = 0;
+        app.data.reflections_day_level = [];
+    };
 
-  app.vue = new Vue({
-    el: '#vue-target',
-    data: function() {
-      return app.data;
-    },
-    methods: app.methods,
-  });
-
-  app.init = () => {};
-
-  app.init();
+    app.init();
 };
 
 init(app);
