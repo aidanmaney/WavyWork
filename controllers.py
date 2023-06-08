b5664404e2ecb84ba1189e7a104773156a495d74
Evin Odisho <evinodisho@gmail.com>
2023-06-08 00:18:19 -0700

    Merge markers were still tehre for some reason...

--- controllers.py
+++ controllers.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 """
 This file defines actions, i.e. functions the URLs are mapped into
 The @action(path) decorator exposed the function at URL:
@@ -160,6 +158,3 @@ def timeline_stage():
     print('DEBUG')
     print(subtasks_list)
     return dict(subtasks_list=subtasks_list)
-
-
->>>>>>> start_merge_from_here
