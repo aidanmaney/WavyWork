32ebef4f9c2c04e1eb430bc0f131a09fc38f27bc
awsmaney1010 <86045868+awsmaney1010@users.noreply.github.com>
2023-06-08 20:42:15 -0700

    Add JS for year for calendar page.

--- static/js/profile.js
+++ static/js/profile.js
@@ -17,6 +17,7 @@ const init = (app) => {
         week5: [],
         week6: [],
         month: "",
+        year: "",
     };
 
     app.enumerate = (a) => {
@@ -60,6 +61,7 @@ const init = (app) => {
                 app.data.week6 = app.data.reflection_blocks.slice(35);
 
                 app.data.month = response.data.month;
+                app.data.year = response.data.year;
             });
     };
 
@@ -92,6 +94,7 @@ const init = (app) => {
     app.init = () => {
         app.data.prevMonthOffset = 0;
         app.data.month = "";
+        app.data.year = "";
         app.data.reflection_blocks = [];
         app.get_reflections();
     };
